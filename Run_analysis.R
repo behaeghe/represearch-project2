c4d065d78a202e3ad026eecaea33f6d08476f01f
JRB <jean.remy@gmail.com>
2016-07-09 09:55:11 -0400

    data loaded and in table format, starting some cleaning activities

--- Run_analysis.R
+++ Run_analysis.R
@@ -12,4 +12,16 @@ if (!file.exists("./data/archive.zip"))
         }
 ## read the data into R
 
-dfstorms <- read.csv("./data/archive.zip",stringsAsFactors = FALSE)
+dfstorms <- read.csv("./data/archive.zip",stringsAsFactors = TRUE)
+## Making the dataframe as table as they are easier to print and manipulate
+library(dplyr)
+storms <- tbl_df(dfstorms)
+## removing the data fram object form memory (> 400MB)
+rm("dfstorms")
+
+##Across the United States, which types of events (as indicated in the 𝙴𝚅𝚃𝚈𝙿𝙴 variabl) are most harmful with respect to population health?
+##Across the United States, which types of events have the greatest economic consequences?
+
+## So we need some processing of our data
+## first let's check how clean our data is
+evtype <- unique(storms$EVTYPE)
